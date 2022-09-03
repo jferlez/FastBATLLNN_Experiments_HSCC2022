3bac71e2191082531156d2a304feb765977bde3a
James Ferlez <jferlez@gmail.com>
2022-09-03 13:36:12 -0700

    Modification to match new poset call signature

--- scripts/TLLReachTester_HSCC_int.py
+++ scripts/TLLReachTester_HSCC_int.py
@@ -86,7 +86,6 @@ def main(args):
                         'method':'fastLP', \
                         'solver':'glpk', \
                         'findAll':False, \
-                        'useBounding':False, \
                         'useQuery':False, \
                         'hashStoreUseBits':True, \
                         'prefilter':False \
